d68496fad3bc2c52cc676c7713a32fbaef9b6c55
GBRBD <gaborattilabodi@gmail.com>
2020-02-08 23:46:03 +0100

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float _speed = 3.5f;
+    [SerializeField] private float speed = 3.5f;
 
     void Start()
     {
@@ -11,12 +11,38 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        CalculateMovement();
+    }
+
+    private void CalculateMovement()
+    {
+        MovePlayer();
+        SetTopAndBottomBoundaries();
+        WarpPlayer();
+    }
+
+    private void MovePlayer()
+    {
         var horizontalInput = Input.GetAxis("Horizontal");
         var verticalInput = Input.GetAxis("Vertical");
-        // transform.Translate(Time.deltaTime * horizontalInput * _speed * Vector3.right);
-        // transform.Translate(Time.deltaTime * verticalInput * _speed * Vector3.up);
-        // Optimal
         var direction = new Vector3(horizontalInput, verticalInput, 0);
-        transform.Translate( _speed * Time.deltaTime * direction);
+        transform.Translate(speed * Time.deltaTime * direction);
+    }
+
+    private void SetTopAndBottomBoundaries()
+    {
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+    }
+
+    private void WarpPlayer()
+    {
+        if (11.3f <= transform.position.x)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
     }
 }
