301781a2c5a56632451ef01fb04729b35eebcec4
GBRBD <gaborattilabodi@gmail.com>
2020-02-09 18:44:23 +0100

    Create spawn routine

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyPrefab;
+    
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    private IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            var posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
+            Instantiate(enemyPrefab, posToSpawn, Quaternion.identity);
+            yield return new WaitForSeconds(5.0f);
+        }
+    }
+}
