07e710c364e1b02d30a911906f77bbb115854217
GBRBD <gaborattilabodi@gmail.com>
2020-02-10 21:34:38 +0100

    Destroy triple shot parent

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -3,12 +3,22 @@
 public class Laser : MonoBehaviour
 {
     [SerializeField] private float speed = 8f;
+    private bool _isParentNotNull;
+
+    private void Start()
+    {
+        _isParentNotNull = transform.parent != null;
+    }
 
     private void Update()
     {
         transform.Translate(speed * Time.deltaTime * Vector3.up);
-        if (8 <= transform.position.y)
+        if (8f <= transform.position.y)
         {
+            if (_isParentNotNull)
+            {
+                Destroy(transform.parent.gameObject);
+            }
             Destroy(this.gameObject);
         }
     }
