d9c504de487dfbb5c957f6b53a102ced27ee21e0
GBRBD <gaborattilabodi@gmail.com>
2020-02-15 19:20:22 +0100

    Create and explode asteroid

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField] private float rotateSpeed = 20.0f;
+    [SerializeField] private GameObject explosionPrefab = default;
+
+    void Update()
+    {
+        transform.Rotate(rotateSpeed * Time.deltaTime * transform.forward);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Laser"))
+        {
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(this.gameObject, 0.1f);
+            Destroy(other.gameObject);
+        }
+    }
+}
